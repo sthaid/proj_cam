b72a8d47bbaef3cc7c7701e444f9a9887e4a9b67
Steven Haid <stevenhaid@gmail.com>
2014-11-28 12:34:54 -0500

    start integrating real viewer in android build

--- android-project/jni/src/Android.mk
+++ android-project/jni/src/Android.mk
@@ -6,18 +6,20 @@ LOCAL_MODULE := main
 
 SDL_PATH := ../SDL2-2.0.3
 SDL_TTF_PATH := ../SDL2_ttf-2.0.12
+SDL_MIXER_PATH := ../SDL2_mixer-2.0.0
 JPEG_PATH := ../jpeg8d
 
-LOCAL_CFLAGS += -DANDROID
+LOCAL_CFLAGS += -DANDROID -fsigned-char
 
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/$(SDL_PATH)/include \
                     $(LOCAL_PATH)/$(SDL_TTF_PATH) \
+                    $(LOCAL_PATH)/$(SDL_MIXER_PATH) \
                     $(LOCAL_PATH)/$(JPEG_PATH)
 
 LOCAL_SRC_FILES := $(SDL_PATH)/src/main/android/SDL_android_main.c \
-	           test.c util.c jpeg_decode.c
+	           viewer.c p2p2.c util.c jpeg_decode.c
 
-LOCAL_SHARED_LIBRARIES := SDL2 SDL2_ttf jpeg
+LOCAL_SHARED_LIBRARIES := SDL2 SDL2_ttf SDL2_mixer myjpeg
 
 LOCAL_LDLIBS := -lGLESv1_CM -lGLESv2 -llog
 
