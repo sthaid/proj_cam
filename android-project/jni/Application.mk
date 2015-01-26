1e07385ce8c5270bec87630f8f6c55e853bcff63
Steven Haid <stevenhaid@gmail.com>
2015-01-25 21:18:00 -0500

    use minSdlVersion equal 12

--- android-project/jni/Application.mk
+++ android-project/jni/Application.mk
@@ -3,6 +3,6 @@
 # See CPLUSPLUS-SUPPORT.html in the NDK documentation for more information
 # APP_STL := stlport_static 
 
-APP_ABI := armeabi-v7a
+APP_ABI := armeabi
 
-APP_PLATFORM := android-19
+APP_PLATFORM := android-12
